e0f732aaa919866e11868bd958a3018b4ee9be55
Wilson Wladimir Lopez Siguencia <wlopez@Todo1of.com.ec>
2022-05-09 16:38:22 -0500

    Adds configuration for discovery an registration service using EUREKA

---
+++ diamonbank-eureka-server/src/main/java/com/diamoncode/diamonbank/eurekaserver/DiamonbankEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.diamoncode.diamonbank.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiamonbankEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamonbankEurekaServerApplication.class, args);
+	}
+
+}
---
+++ diamonbank-eureka-server/src/test/java/com/diamoncode/diamonbank/eurekaserver/DiamonbankEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamoncode.diamonbank.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamonbankEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
