ea335bc4641309c974942ec271ff2173a44f9ea4
Wladimir Lopez <wllopez@technisys.com>
2023-05-08 14:01:21 -0500

    generacion de un apigateway

---
+++ diamonbank-apigateway/src/main/java/com/diamoncode/diamonbankapigateway/DiamonbankApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.diamoncode.diamonbankapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamonbankApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiamonbankApigatewayApplication.class, args);
+    }
+
+}
---
+++ diamonbank-apigateway/src/test/java/com/diamoncode/diamonbankapigateway/DiamonbankApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamoncode.diamonbankapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamonbankApigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
