e8150432549e3843ec441e29374cb6513b95f186
WladeCode <warriorwl86@gmail.com>
2025-10-02 20:03:53 -0500

    add rol converter in api gateway to get roles

--- diamonbank-apigateway/src/main/java/com/diamoncode/diamonbankapigateway/config/SecurityConfig.java
+++ diamonbank-apigateway/src/main/java/com/diamoncode/diamonbankapigateway/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.diamoncode.diamonbankapigateway.config;
 
+import com.diamoncode.diamonbankapigateway.config.converter.RoleConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -12,13 +14,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(exchanges -> exchanges
-                .pathMatchers("/actuator/**").permitAll()
-                .pathMatchers("/dc-api/accounts/**").authenticated()
-                .pathMatchers("/dc-api/cards/**").authenticated()
-                .pathMatchers("/dc-api/loans/**").permitAll()
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new RoleConverter());
 
-        ).oauth2ResourceServer().jwt();
+        http.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/actuator/**").permitAll()
+                        .pathMatchers("/dc-api/accounts/**").authenticated()
+                        .pathMatchers("/dc-api/customer/**").authenticated()
+                        .pathMatchers("/dc-api/cards/**").authenticated()
+                        .pathMatchers("/dc-api/loans/**").permitAll())
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt());
 
         http.csrf().disable();
         return http.build();
